9e1335d84e02cb3259a60fec7024a12d7ebec60c
Yagel <yagelsharfi@gmail.com>
2020-12-19 18:43:43 +0200

    ready for release 3.0!

--- src/main/java/club/cafedevelopment/blitz/event/Event.java
+++ src/main/java/club/cafedevelopment/blitz/event/Event.java
@@ -1,5 +1,6 @@
 package club.cafedevelopment.blitz.event;
 
+import club.cafedevelopment.blitz.dispatcher.DispatcherEntry;
 import club.cafedevelopment.blitz.modifiers.EventEra;
 
 /**
@@ -9,9 +10,16 @@ public abstract class Event {
     private boolean cancelled = false;
     public final EventEra era;
 
-    public Event(EventEra era) {
-        this.era = era;
-    }
+    /**
+     * will be used when you will make an event that does have eras
+     * @param era the era for the event
+     */
+    public Event(EventEra era) { this.era = era; }
+
+    /**
+     * will be used when you will make an event that does not have eras, as {@link EventEra#PRE} is the default for {@link DispatcherEntry#era()}
+     */
+    public Event() { this.era = EventEra.PRE; }
 
     /**
      * @return is the event cancelled
